e3e086a12629077fb18b836c6a516d2a5c4b8e55
dimitrivinet <dimitri.vinet@edu.devinci.fr>
2021-09-03 17:15:13 +0200

    AI lesson 1

--- AI/AI01/src/main.py
+++ AI/AI01/src/main.py
@@ -1,24 +1,98 @@
+from torch.utils.data.dataloader import DataLoader
+from torchvision.datasets.cifar import CIFAR10
+from torchvision.datasets.mnist import MNIST
+from efficientnet_pytorch import EfficientNet
+
 import modules.models
 import modules.train
+from modules.data import PIL_to_tensor, CIFAR_transform, TrainingData
 
 N_CLASSES = 10
 DATASET_DIR = "/home/dimitri/Documents/A5-Courses/AI/AI01/dataset"
 SAVE_PATH = "/home/dimitri/Documents/A5-Courses/AI/AI01/output"
 NUM_EPOCHS = 100
-SAVE_ALL=False
+SAVE_ALL = False
+# DEVICE = "cuda:0"
+DEVICE="cpu"
+
 
 def main():
-    model = modules.models.MLP(N_CLASSES)
+    # model = modules.models.MLP(N_CLASSES)
     # model = modules.models.LeNet5(N_CLASSES)
     # model = modules.models.VGG16(N_CLASSES)
     # model = modules.models.ResNet15(N_CLASSES)
+    model = model = EfficientNet.from_pretrained('efficientnet-b1', num_classes=N_CLASSES)
 
+    trainset_MNIST = MNIST(
+        root=DATASET_DIR,
+        train=True,
+        transform=PIL_to_tensor,
+        target_transform=None,
+        download=True
+    )
+    validset_MNIST = MNIST(
+        root=DATASET_DIR,
+        train=False,
+        transform=PIL_to_tensor,
+        target_transform=None,
+        download=True
+    )
+    training_data_MNIST = TrainingData(trainset=trainset_MNIST,
+                                       validset=validset_MNIST,
+                                       trainloader=DataLoader(
+                                           trainset_MNIST,
+                                           batch_size=32,
+                                           shuffle=True,
+                                           num_workers=2,
+                                           pin_memory=True,
+                                       ),
+                                       validloader=DataLoader(
+                                           validset_MNIST,
+                                           batch_size=32,
+                                           shuffle=True,
+                                           num_workers=2,
+                                           pin_memory=True,
+                                       ))
+
+    trainset_CIFAR = CIFAR10(
+        root=DATASET_DIR,
+        train=True,
+        transform=CIFAR_transform,
+        target_transform=None,
+        download=True
+    )
+    validset_CIFAR = CIFAR10(
+        root=DATASET_DIR,
+        train=False,
+        transform=CIFAR_transform,
+        target_transform=None,
+        download=True
+    )
+    training_data_CIFAR = TrainingData(trainset=trainset_CIFAR,
+                                       validset=validset_CIFAR,
+                                       trainloader=DataLoader(
+                                           trainset_CIFAR,
+                                           batch_size=32,
+                                           shuffle=True,
+                                           num_workers=2,
+                                           pin_memory=True,
+                                       ),
+                                       validloader=DataLoader(
+                                           validset_CIFAR,
+                                           batch_size=32,
+                                           shuffle=True,
+                                           num_workers=2,
+                                           pin_memory=True,
+                                       ))
 
     modules.train.train(model=model,
-        dataset_dir=DATASET_DIR,
-        save_path=SAVE_PATH,
-        num_epochs=NUM_EPOCHS,
-        save_all=SAVE_ALL)
+                        # training_data=training_data_MNIST,
+                        training_data=training_data_CIFAR,
+                        save_path=SAVE_PATH,
+                        num_epochs=NUM_EPOCHS,
+                        save_all=SAVE_ALL,
+                        device=DEVICE)
+
 
 if __name__ == "__main__":
     main()
--- AI/AI01/src/modules/data.py
+++ AI/AI01/src/modules/data.py
@@ -1,50 +1,24 @@
+from dataclasses import dataclass
+
 import numpy as np
 import PIL.Image
 import torch
 from torch.utils.data import DataLoader, Dataset
-from torchvision.datasets import MNIST
 
 
 def PIL_to_tensor(image: PIL.Image):
     im_torch = torch.tensor(np.array(image, dtype=np.float32))
     return im_torch
 
+def CIFAR_transform(image: PIL.Image):
+    x = PIL_to_tensor(image)
+    x = torch.permute(x, (2, 0, 1))
 
+    return x
+
+@dataclass
 class TrainingData():
     trainset: Dataset
     validset: Dataset
     trainloader: DataLoader
     validloader: DataLoader
-
-    def __init__(self, dataset_dir):
-        self.trainset = MNIST(
-            root=dataset_dir,
-            train=True,
-            transform=PIL_to_tensor,
-            target_transform=None,
-            download=True
-        )
-
-        self.validset = MNIST(
-            root=dataset_dir,
-            train=False,
-            transform=PIL_to_tensor,
-            target_transform=None,
-            download=True
-        )
-
-        self.trainloader = DataLoader(
-            self.trainset,
-            batch_size=32,
-            shuffle=True,
-            num_workers=6,
-            pin_memory=True,
-        )
-
-        self.validloader = DataLoader(
-            self.validset,
-            batch_size=32,
-            shuffle=True,
-            num_workers=6,
-            pin_memory=True,
-        )
